6b0520c905304aa0852ad70fbb277445e105480e
ianq1w1 <ianpereiraian2@gmail.com>
2025-12-14 01:38:05 -0300

    initial commit

---
+++ apipizza/src/main/java/com/example/apipizza/ApipizzaApplication.java
@@ -0,0 +1,13 @@
+package com.example.apipizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApipizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApipizzaApplication.class, args);
+	}
+
+}
---
+++ apipizza/src/test/java/com/example/apipizza/ApipizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apipizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApipizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
