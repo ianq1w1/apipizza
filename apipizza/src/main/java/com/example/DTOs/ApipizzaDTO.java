f9d0aae4b8382963f91c343ee43ef6e266026a4d
ianq1w1 <ianpereiraian2@gmail.com>
2026-03-07 20:37:00 -0300

    tentando consertar o fluxo das coisas

---
+++ apipizza/src/main/java/com/example/DTOs/ApipizzaDTO.java
@@ -0,0 +1,8 @@
+package com.example.DTOs;
+
+
+
+public record ApipizzaDTO(String sabor) {
+}
+   
+
--- apipizza/src/main/java/com/example/apipizza_Controller/ApipizzaController.java
+++ apipizza/src/main/java/com/example/apipizza_Controller/ApipizzaController.java
@@ -1,11 +1,17 @@
 package com.example.apipizza_Controller;
 
-import com.example.apipizzaModel.ApipizzaModel;
+//import com.example.apipizzaModel.ApipizzaModel;
+import com.example.DTOs.ApipizzaDTO;
 import com.example.repositories.ApipizzaRepository;
+
+//import com.example.repositories.ApipizzaRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 //import com.example.repositories.ApipizzaRepository;
 
@@ -19,7 +25,14 @@ public class ApipizzaController {
         this.repository = repository;
     }
 
-
+    @PostMapping("/post")
+    public String createPizza(@RequestBody String sabor) {
+        //TODO: process POST request
+        this.
+        
+        return entity;
+    }
+    
     @GetMapping("/pizza/{id}")
     ApipizzaModel one(@PathVariable Long id) {
 
