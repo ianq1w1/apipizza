f14c18b21f1735ea79d6e063ec70fcd11bb14e7d
ianq1w1 <ianpereiraian2@gmail.com>
2026-03-31 04:00:01 -0300

    mais adições referentes ao spring security

--- apipizza/src/main/java/com/example/apipizza/apipizzaService/UserService.java
+++ apipizza/src/main/java/com/example/apipizza/apipizzaService/UserService.java
@@ -3,23 +3,32 @@ package com.example.apipizza.apipizzaService;
 import org.apache.catalina.User;
 import org.hibernate.id.uuid.UuidVersion7Strategy;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.function.EntityResponse;
 import jakarta.transaction.Transactional;
 
 
 import com.example.apipizza.DTOs.UserResponseDTO;
+import com.example.apipizza.DTOs.UserResponseLoginDTO;
 import com.example.apipizza.DTOs.UserRequestDTO;
+import com.example.apipizza.DTOs.UserLoginDTO;
+import com.example.apipizza.DTOs.UserResponseDTO;
 import com.example.apipizza.repositories.UserRepository;
 import com.example.apipizza.apipizzaModel.userEntity;
+import com.example.apipizza.apipizzaService.TokenService;
 
 
 @Service
 public class UserService {
 
     private final UserRepository repository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
 
-    public UserService (UserRepository repository){
+    public UserService (UserRepository repository, BCryptPasswordEncoder passwordEncoder, TokenService tokenService){
         this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+        this.tokenService = tokenService;
     }
     
     //criar
@@ -30,12 +39,24 @@ public class UserService {
         user.setEmail(dto.email());
         user.setNome(dto.nome());
 
-        //depois aplica o bcrypt com spring security!!!
-        user.setPassword(dto.password());
+        user.setPassword(passwordEncoder.encode(dto.password())); // BCrypt aqui
+  
         userEntity userSalvo = repository.save(user);
 
         return new UserResponseDTO(userSalvo.getEmail(), userSalvo.getNome());   
     }
 
+    public UserResponseLoginDTO login(UserLoginDTO dto){
+        userEntity user = repository.findByEmail(dto.email())
+            .orElseThrow(() -> new RuntimeException("nao achamo esse email nao ein"));
+        
+        if(!passwordEncoder.matches(dto.password(), user.getPassword())){
+            throw new RuntimeException("nao bateu a senha");
+        }
+
+        String token = tokenService.gerarToken(user);
+
+        return new UserResponseLoginDTO(token, user.getEmail(), user.getNome());
+    }
     
 }
